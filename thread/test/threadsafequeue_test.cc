0d09ed4835b937c83520cbb88072024d4cc86845
Tigran Hakobyan <tigrandp@gmail.com>
2018-05-18 13:06:53 -0400

    Added synchronized wait queue.

--- thread/test/threadsafequeue_test.cc
+++ thread/test/threadsafequeue_test.cc
@@ -1,6 +1,7 @@
 #include "thread/threadsafequeue.h"
 
 #include <algorithm>
+#include <chrono>
 #include <functional>
 #include <thread>
 
@@ -12,7 +13,8 @@ namespace {
 
 using NumberRange = std::pair<int, int>;
 
-void PushSingleRange(const NumberRange& range, SynchronizedQueue<int>* q) {
+template<typename Q>
+void PushSingleRange(const NumberRange& range, Q* q) {
   for (int i = range.first; i <= range.second; ++i) {
     q->PushBack(i);
     if (i % 10 == 0) {
@@ -23,8 +25,8 @@ void PushSingleRange(const NumberRange& range, SynchronizedQueue<int>* q) {
   }
 }
 
-void PushRanges(const std::vector<NumberRange>& ranges,
-                SynchronizedQueue<int>* q) {
+template<typename Q>
+void PushRanges(const std::vector<NumberRange>& ranges, Q* q) {
   std::vector<int> indexes;
   for (int i = 0; i < ranges.size(); ++i) {
     indexes.push_back(i);
@@ -32,7 +34,7 @@ void PushRanges(const std::vector<NumberRange>& ranges,
   std::random_shuffle(indexes.begin(), indexes.end());
   std::vector<std::thread> threads;
   for (int idx : indexes) {
-    threads.emplace_back(PushSingleRange, std::ref(ranges[idx]), q);
+    threads.emplace_back(PushSingleRange<Q>, std::ref(ranges[idx]), q);
   }
   std::for_each(threads.begin(), threads.end(),
                 std::mem_fn(&std::thread::join));
@@ -76,6 +78,111 @@ TEST(ThreadSafeQueueTest, ThreadSafeStackSemantics) {
     all_range_numbers.erase(poped);
   }
   EXPECT_TRUE(all_range_numbers.empty());
+}
+
+TEST(ThreadSafeQueueTest, WaitQueueGeneralUse) {
+  WaitSyncQueue<int> q;
+  std::vector<NumberRange> ranges = {
+    {0, 100}, {500, 600}, {1000, 2000}, {10000, 15000}};
+  PushRanges(ranges, &q);
+
+  std::set<int> all_range_numbers;
+  for (const NumberRange& range : ranges) {
+    for (int i = range.first; i <= range.second; ++i) {
+      all_range_numbers.insert(i);
+    }
+  }
+
+  int popped = -1;
+  while (!all_range_numbers.empty()) {
+    q.PopBack(&popped);
+    EXPECT_EQ(all_range_numbers.count(popped), 1);
+    all_range_numbers.erase(popped);
+  }
+}
+
+// Test scenario which exercises that WaitQueue awaits for elements to appear
+// if it is empty.
+TEST(ThreadSafeQueueTest, WaitsElementsToAppearBeforePop) {
+  constexpr int kPushesPerThread = 7;
+  constexpr int kNumThreads = 3;
+
+  WaitSyncQueue<int> q;
+  
+  // Function which sleeps for a second and pushes element to queue.
+  auto pusher_thread = [&q](int iterations) {
+    for (int it = 0; it < iterations; ++it) {
+      q.PushBack(7);
+      std::this_thread::sleep_for(std::chrono::duration<int, std::milli>());
+    }
+  };
+
+  std::vector<std::thread> threads;
+  for (int i = 0; i < kNumThreads; ++i) {
+    threads.emplace_back(pusher_thread, kPushesPerThread);
+  }
+
+  for (int i = 0; i < kPushesPerThread * kNumThreads; ++i) {
+    int popped = -1;
+    q.PopBack(&popped);
+    EXPECT_EQ(popped, 7);
+  }
+  std::for_each(threads.begin(), threads.end(),
+                std::mem_fn(&std::thread::join));
+}
+
+// Test scenario which exercises that WaitQueue waits for elements to be
+// consumed before pushing new ones.
+TEST(ThreadSafeQueueTest, WaitsElementsToBeConsumed) {
+  constexpr int kConsumersPerThread = 7;
+  constexpr int kNumThreads = 3;
+
+  WaitSyncQueue<int> q;
+  q.SetMaxQueueSize(21);
+  for (int i = 0; i < kConsumersPerThread * kNumThreads; ++i) {
+    q.PushBack(1);
+  }
+  
+  // Function which sleeps for a second and pushes element to queue.
+  auto consumer_thread = [&q](int iterations) {
+    for (int it = 0; it < iterations; ++it) {
+      int popped;
+      q.PopFront(&popped);
+      std::this_thread::sleep_for(std::chrono::duration<int, std::milli>());
+    }
+  };
+
+  std::vector<std::thread> threads;
+  for (int i = 0; i < kNumThreads; ++i) {
+    threads.emplace_back(consumer_thread, kConsumersPerThread);
+  }
+
+  for (int i = 0; i < kConsumersPerThread * kNumThreads; ++i) {
+    q.PushBack(-1);
+  }
+  for (int i = 0; i < kConsumersPerThread * kNumThreads; ++i) {
+    int popped;
+    q.PopBack(&popped);
+    EXPECT_EQ(-1, popped);
+  }
+  std::for_each(threads.begin(), threads.end(),
+                std::mem_fn(&std::thread::join));
+}
+
+// Test scenario which exercises that all the push waiters are correctly
+// stopped.
+TEST(ThreadSafeQueueTest, StopsAllPushWaiters) {
+  WaitSyncQueue<int> q;
+  q.SetMaxQueueSize(0);
+  bool result = true;
+  std::thread t([&q, &result]() { result = q.PushBack(1); });
+  
+  // Make sure that thread have already started.
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+  q.StopAllWaiters();
+  t.join();
+
+  EXPECT_FALSE(result);
 }
 
 }  // namespace
--- thread/threadsafequeue.h
+++ thread/threadsafequeue.h
@@ -1,6 +1,7 @@
 #ifndef THREAD_THREADSAFEQUEUE_H_
 #define THREAD_THREADSAFEQUEUE_H_
 
+#include <condition_variable>
 #include <deque>
 #include <mutex>
 
@@ -9,7 +10,8 @@ namespace thread {
 // Queue which synchronizes all the operations using only locking.
 // Queue semantics are provided by PushBack/PopFront, stack semantics by
 // PushBack/PopBack. All functions are atomic.
-template<typename T> class SynchronizedQueue {
+template <typename T>
+class SynchronizedQueue {
  public:
   using ValueType = T;
   using ContainerType = std::deque<T>;
@@ -51,7 +53,169 @@ template<typename T> class SynchronizedQueue {
   ContainerType elements_;
 };
 
+// Thread safe queue which allows waiting for elements to appear or to be
+// consumed. All the operations are atomic.
+template <typename T>
+class WaitSyncQueue {
+ public:
+  using ValueType = T;
+  using ContainerType = std::deque<T>;
+  using SizeType = typename ContainerType::size_type;
+
+  WaitSyncQueue() {}
+
+  bool Empty() const {
+    std::lock_guard<std::mutex> lk(beasy_);
+    return elements_.empty();
+  }
+
+  SizeType Size() const {
+    std::lock_guard<std::mutex> lk(beasy_);
+    return elements_.size();
+  }
+
+  // Sets the maximal number of elements that queue can hold, by default value
+  // is inifinity. If the queue is full and some insertion was requeusted
+  // thread will be blocked until the queue becomes unfull again.
+  void SetMaxQueueSize(SizeType new_max_queue_size) {
+    std::lock_guard<std::mutex> lk(beasy_);
+    max_queue_size_ = new_max_queue_size;
+  }
+
+  SizeType MaxQueueSize() const {
+    std::lock_guard<std::mutex> lk(beasy_);
+    return max_queue_size_;
+  }
+
+  // Appends the given element to the end of the container, if container
+  // already reached its maximum capacity thread will wait until some of the
+  // elements is consumed, if during the wait period waiters stop was requested
+  // it will return false otherwise true.
+  bool PushBack(const ValueType& value);
+
+  // Inserts the given element to the beginning of the container, if container
+  // is already reached it maximum capacity thread will wait until some of the
+  // elements is consumed, if during the wait period waiters stop was requested
+  // it will return false otherwise true.
+  bool PushFront(const ValueType& value);
+
+  // Removes the element from the back of the container and copies that value
+  // to the given output, if queue is empty then thread will be blocked until
+  // some elements appear, if during the wait period waiters stop was requested
+  // it will return false otherwise true.
+  bool PopBack(ValueType* result);
+
+  // Removes the element from the front of the container and copies that value
+  // to the given output, if queue is empty then thread will be blocked until
+  // some elements appear, if during the wait period waiters stop was requested
+  // it will return false otherwise true.
+  bool PopFront(ValueType* result);
+
+  // Effective way of copying the content of the queue.
+  void SwapWithEmpty(ContainerType* other);
+
+  // Stops all the waiters.
+  void StopAllWaiters();
+
+ private:
+  void PushUnsafe(const ValueType& value, bool push_to_back) {
+    if (push_to_back) {
+      elements_.push_back(value);
+    } else {
+      elements_.push_front(value);
+    }
+  }
+
+  void PopUnsafe(ValueType* result, bool pop_from_back) {
+    if (pop_from_back) {
+      *result = elements_.back();
+      elements_.pop_back();
+    } else {
+      *result = elements_.front();
+      elements_.pop_front();
+    }
+  }
+
+  bool PopInternal(ValueType* result, bool pop_from_back);
+  bool PushInternal(const ValueType& value, bool push_to_back);
+
+  mutable std::mutex beasy_;
+  ContainerType elements_;
+
+  std::condition_variable queue_not_full_;
+  std::condition_variable queue_not_empty_;
+
+  bool stop_requested_ = false;
+  // Since size type is unsigned this will end up being infinity.
+  SizeType max_queue_size_ = -1;
+};
+
 template<typename T>
+void WaitSyncQueue<T>::StopAllWaiters() {
+  std::lock_guard<std::mutex> lk(beasy_);
+  stop_requested_ = true;
+  queue_not_full_.notify_all();
+  queue_not_empty_.notify_all();
+}
+
+template<typename T>
+void WaitSyncQueue<T>::SwapWithEmpty(ContainerType* other) {
+  assert(other != nullptr);
+  assert(other->empty());
+  std::lock_guard<std::mutex> lk(beasy_);
+  elements_.swap(*other);
+  queue_not_full_.notify_all();
+}
+
+template <typename T>
+bool WaitSyncQueue<T>::PopInternal(ValueType* result, bool pop_from_back) {
+  std::unique_lock<std::mutex> lk(beasy_);
+  while (elements_.empty()) {
+    queue_not_empty_.wait(lk);
+    if (stop_requested_) return false;
+  }
+
+  PopUnsafe(result, pop_from_back);
+  if (elements_.size() < max_queue_size_) {
+    queue_not_full_.notify_one();
+  }
+  return true;
+}
+
+template <typename T>
+bool WaitSyncQueue<T>::PopBack(ValueType* result) {
+  return PopInternal(result, true /* pop_from_back */);
+}
+
+template <typename T>
+bool WaitSyncQueue<T>::PopFront(ValueType* result) {
+  return PopInternal(result, false /* pop_from_back */);
+}
+
+template <typename T>
+bool WaitSyncQueue<T>::PushInternal(const ValueType& value, bool push_to_back) {
+  std::unique_lock<std::mutex> lk(beasy_);
+  while (elements_.size() >= max_queue_size_) {
+    queue_not_full_.wait(lk);
+    if (stop_requested_) return false;
+  }
+
+  if (elements_.empty()) queue_not_empty_.notify_one();
+  PushUnsafe(value, push_to_back);
+  return true;
+}
+
+template <typename T>
+bool WaitSyncQueue<T>::PushBack(const ValueType& value) {
+  return PushInternal(value, true /* push_to_back */);
+}
+
+template <typename T>
+bool WaitSyncQueue<T>::PushFront(const ValueType& value) {
+  return PushInternal(value, false /* push_to_back */);
+}
+
+template <typename T>
 bool SynchronizedQueue<T>::PopBack(ValueType* top) {
   std::lock_guard<std::mutex> lk(m_);
   if (elements_.empty()) return false;
@@ -60,7 +224,7 @@ bool SynchronizedQueue<T>::PopBack(ValueType* top) {
   return true;
 }
 
-template<typename T>
+template <typename T>
 bool SynchronizedQueue<T>::PopFront(ValueType* front) {
   std::lock_guard<std::mutex> lk(m_);
   if (elements_.empty()) return false;
@@ -69,11 +233,11 @@ bool SynchronizedQueue<T>::PopFront(ValueType* front) {
   return true;
 }
 
-template<typename T>
+template <typename T>
 void SynchronizedQueue<T>::SwapWithEmpty(ContainerType* other) {
   assert(other->empty());
   std::lock_guard<std::mutex> lk(m_);
-  elements_.swap(other);
+  elements_.swap(*other);
 }
 
 }  // namespace thread
